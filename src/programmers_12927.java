ee9ea74626ec1877173cbd3a50365fa5c9bafb3c
skhu202112021-creator <skhu202112021@gmail.com>
2026-04-07 17:01:03 +0900

    2026-04-07

---
+++ src/programmers_12927.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class Solution {
+	public static long programmers_12927(int n, int[] works) {
+		long answer = 0;
+		int max = 0;
+		int index = works.length - 1;
+		int [] dp = new int[works.length];
+		Arrays.sort(works);
+		while(n != 0) {
+			if(index-1 != -1) {
+				if(works[index] < works[index-1]) {
+					dp[index] = works[index];
+					index--;
+				}
+			}
+			int count = works.length - 1;
+			while(count != 0 && dp[count] != 0) {
+				if(works[index] < dp[count]) {
+					index = count;
+				}
+				count--;
+				while(dp[count] != 0) {
+					count--;
+				}
+			}
+			max = works[index];
+			int x = max - 1;
+			if (x == -1) {
+				return 0;
+			}
+			works[index] = x;
+			n--;
+		}
+		for (int i = 0; i < works.length; ++i) {
+			answer += Math.pow(works[i], 2);
+		}
+		return answer;
+	}
+}
